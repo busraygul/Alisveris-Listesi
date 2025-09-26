0f6b7691c794acd8b9cbc0b78844f28e92443407
busraygul <aygul.busra58@gmail.com>
2025-09-26 17:06:09 +0300

    canlı

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/Alisveris-Listesi/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
