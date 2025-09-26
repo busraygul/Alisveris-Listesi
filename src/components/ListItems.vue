cabd2d37aa07d24e38c882039f331b8e55f66ef7
busraygul <aygul.busra58@gmail.com>
2025-09-26 15:50:46 +0300

    filtreleme yapıldı

--- src/components/ListItems.vue
+++ src/components/ListItems.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed } from 'vue'
+import { ref, computed } from 'vue'
 import { useStore } from 'vuex'
 import ListItem from './ListItem.vue'
 
@@ -8,10 +8,37 @@ const items = computed(() => store.getters.items)
 const total = computed(() => store.getters.total)
 const doneCount = computed(() => store.getters.doneCount)
 
+const filter = ref('all')
+const filteredItems = computed(() => {
+    if (filter.value === 'done') return items.value.filter(i => i.done)
+    if (filter.value === 'todo') return items.value.filter(i => !i.done)
+    return items.value
+})
 </script>
 
 <template>
     <div>
+        <div class="filters">
+            <div class="flex gap-2">
+                <!-- Hepsi -->
+                <button @click="filter = 'all'" class="px-3 py-1 rounded-md border text-sm"
+                    :class="filter === 'all' ? 'bg-rose-500 text-white border-rose-500' : 'bg-white border-rose-300'">
+                    Hepsi
+                </button>
+
+                <!-- Satın alınmadı -->
+                <button @click="filter = 'todo'" class="px-3 py-1 rounded-md border text-sm"
+                    :class="filter === 'todo' ? 'bg-rose-500 text-white border-rose-500' : 'bg-white border-rose-300'">
+                    Satın alınmadı
+                </button>
+
+                <!-- Satın alındı -->
+                <button @click="filter = 'done'" class="px-3 py-1 rounded-md border text-sm"
+                    :class="filter === 'done' ? 'bg-rose-500 text-white border-rose-500' : 'bg-white border-rose-300'">
+                    Satın alındı
+                </button>
+            </div>
+        </div>
         <div class="text-right mb-2">
             <small>Satın Alınan Ürün: {{ doneCount }} </small>
             <br>
@@ -19,7 +46,7 @@ const doneCount = computed(() => store.getters.doneCount)
             <br>
         </div>
         <ul class="space-y-1">
-            <ListItem v-for="it in items" :key="it.id" :item="it" />
+            <ListItem v-for="it in filteredItems" :key="it.id" :item="it" />
         </ul>
     </div>
 </template>
